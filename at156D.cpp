ffadb63d28a4078e1919eddbfaee241f36821bb9
TTQ <1932170807@qq.com>
2020-02-29 09:18:33 +0800

    [fix]at156D

--- at156D.cpp
+++ at156D.cpp
@@ -8,57 +8,46 @@
 #define fast_io()                \
     ios::sync_with_stdio(false); \
     std::cin.tie(0);
+#define mod 1000000007
 typedef long long ll;
 typedef long double ld;
 using namespace std;
-#define G  1100000  
-#define mod 1000003  
-ll pri[G];  
-ll ni[G],ans;  
-ll poww(ll a,ll b)  
-{  
-    ll ans=1,base=a;  
-    while (b>0)  
-    {  
-        if (b%2==1)  
-            ans=(base*ans)%mod;  
-        base=(base*base)%mod;  
-        b/=2;  
-    }  
-    return ans;  
-}  
-void s()   //打表 
-{  
-    pri[0]=1;  
-    ni[0]=1;  
-    for (int i=1;i<G ;i++)  
-    {  
-        pri[i]=pri[i-1]*i%mod;  //N！ 
-        ni[i]=poww(pri[i],mod-2);  
-    }  
-}  
-
+ll qpow(ll a,ll b,ll m)
+{
+    ll ans=1;
+    ll base = a;
+    while(b>0)
+    {
+        if(b&1)
+            ans = (ans * base) % m;
+        base = (base * base) % m;
+        b >>= 1;
+    }
+    return ans;
+}
 ll C(ll n,ll m)
 {
-    return ((pri[n]*ni[m]%mod)*ni[n-m])%mod;
+    ll ans = 1;
+    for (ll i = 1;i<=m; i++)
+    {
+        ll tmp;
+        tmp = (n - i + 1) % mod;
+        ans *= (tmp * qpow(i, mod - 2, mod)) % mod;
+        ans %= mod;
+    }
+    return ans;
 }
 
 int main()
 {
     fast_io();
-    s();
     ll n, a, b;
     cin >> n >> a >> b;
     ll ans;
-    ans = poww(2, n);
-    cout << ans << endl;
-    ans += 2 * mod - 1;
-    ans-=C(n, a);
-    
-    ans-=C(n, b);
-    cout << ans % mod;
-
-    
+    ans = qpow(2, n, mod) - 1;
+    ans=(ans+mod-C(n, a))%mod;
+    ans=(ans+mod-C(n, b))%mod;
+    cout << ans;
     system("pause");
     return 0;
 }
