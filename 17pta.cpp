dfcd7328c937249b51b555dbad2c4d322baf9b14
TTQ <1932170807@qq.com>
2020-03-29 20:38:10 +0800

    17pta12

--- 17pta.cpp
+++ 17pta.cpp
@@ -22,52 +22,16 @@ using namespace std;
 int main()
 {
     fast_io();
-    int n;
-    double z, r, sum = 0.0;
-    cin >> n >> z >> r;
-    r = (1 - r / 100.0);
-    int dedao[100001];
-    vector<int> td[100001];
-    for(int i=0; i<n; i++)
-    {
-        int k, x;
-        cin >> k;
-        if(!k)
-        {
-            cin >> x;
-            dedao[i] = x;
-        }
-        else
-        {
-            
-            for (int j = 0; j < k;j++)
-            {
-                cin >> x;
-                td[i].push_back(x);
-            }
-        }
-    }
-    int t;
-    int list[100001];
+    int w, p;
+    cin >> w >> p;
+    double vv = 200000.0 / w;
+    p = 100 - p;
     double ans = 0;
-    queue<int> q;
-    q.push(0);
-    list[0] = 0;
-    while (!q.empty())
+    while (vv > 1e-6)
     {
-        t=q.front();
-        q.pop();
-        for (int i = 0; i <td[t].size();i++)
-        {
-            list[td[t][i]] = list[t] + 1;
-            if(dedao[td[t][i]])
-                ans += z * pow(r, list[td[t][i]]) * dedao[td[t][i]];
-            else
-                q.push(td[t][i]);
-        }
+        ans += vv / 9.8;
+        vv *= p * 1.0 / 100;
     }
-    if(n==1&&dedao[0])
-        ans = z * dedao[0];
-    cout << (int)ans;
+    cout << fixed << setprecision(3) << ans;
     return 0;
 }
