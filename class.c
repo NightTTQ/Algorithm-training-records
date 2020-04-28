2c402a365314229a1c2c348eeb3b16b503f13abe
TTQ <1932170807@qq.com>
2020-04-28 20:29:46 +0800

    class task

--- class.c
+++ class.c
@@ -1,8 +1,18 @@
 #include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
+float sum(int n, int m)
+{
+    float s = 0;
+    for (int i = n; i <= m; i++)
+        s += (float)1 / i;
+    return s;
+}
 
 int main()
 {
-    char c[6];
+    int n, m, ans = 0;
+    scanf("%d%d", &n, &m);
+    printf("%f", sum(n, m));
+    return 0;
 }
