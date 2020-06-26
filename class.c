fc6d47ac85c46162e7b5095d51c05426472baa35
TTQ <1932170807@qq.com>
2020-06-26 09:47:31 +0800

    PrimeFactor

--- class.c
+++ class.c
@@ -1,11 +1,36 @@
 #include <stdio.h>
+#include <string.h>
+
+int pri[100000], vis[100000];
+
+int primeFactor(int n)
+{
+    int cnt = 0;
+    for (int i = 2; i <= n; i++)
+    {
+        if (!vis[i])
+        {
+            pri[cnt++] = i;
+            vis[i] = 1;
+        }
+        for (int j = 0; j < cnt; j++)
+        {
+            if (pri[j] * i > n)
+                break;
+            vis[i * pri[j]] = 1;
+            if (!(i % pri[j]))
+                break;
+        }
+    }
+    return cnt;
+}
 
 int main()
 {
-    int n = 2, k = 0;
-    while (k++ && n++ > 2)
-        ;
-    printf("% d % d\n", k, n);
-
+    int n;
+    memset(vis, 0, sizeof(vis));
+    memset(pri, 0, sizeof(pri));
+    scanf("%d", &n);
+    printf("%d\n", primeFactor(n));
     return 0;
 }
