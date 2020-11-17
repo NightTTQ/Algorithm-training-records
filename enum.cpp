9913d120d925ab90554ee3e35672a77e33e50448
TTQ <1932170807@qq.com>
2020-11-17 21:13:47 +0800

    enum

---
+++ enum.cpp
@@ -0,0 +1,43 @@
+//#include <bits/stdc++.h>
+#include <algorithm>
+#include <cmath>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <functional>
+#include <iomanip>
+#include <iostream>
+#include <map>
+#include <queue>
+#include <set>
+#include <stack>
+#include <string>
+#include <vector>
+#define fast_io()                \
+    ios::sync_with_stdio(false); \
+    std::cin.tie(0);
+#define mod 998244353
+typedef long long ll;
+typedef long double ld;
+using namespace std;
+
+enum num
+{
+    a1 = 2,
+    a2,
+    a3
+};
+
+int main()
+{
+    fast_io();
+    num a4 = a1;
+    cout << a1 << " " << a2 << " " << a3 << "\n";
+    cout << a4;
+    {
+        const static int sta = 111;
+        cout << sta;
+    }
+
+    return 0;
+}
